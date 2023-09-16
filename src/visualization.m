5a25ce53f43b5cc91602e8e54e3bcf2a0599a3d3
Leevi <leewi.982@hotmail.com>
2023-09-16 11:19:28 +0300

    Added file for visualization.

---
+++ src/visualization.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = visualization(inputArg1,inputArg2)
+%VISUALIZATION Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
